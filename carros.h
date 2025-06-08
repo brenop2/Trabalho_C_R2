bb22e0bfcdb30a731ea7921b47fac943d45a5435
brenop2 <breno.p231@gmail.com>
2025-06-07 23:27:13 -0300

    subindo aterações

--- carros.h
+++ carros.h
@@ -1,32 +1,34 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
-#include "carros.h"
+#ifndef CARROS_H
+#define CARROS_H
 
-int main(int argc, char* argv[]) {
-    if (argc != 3) {
-        printf("Uso: %s <entrada.csv> <saida.bin>\n", argv[0]);
-        return 1;
-    }
+typedef struct {
+    char marca[21];
+    char modelo[31];
+    int ano;
+    int km;
+    float preco;
+} Carro;
 
-    FILE* f = fopen(argv[1], "r");
-    if (!f) {
-        printf("Erro ao abrir %s\n", argv[1]);
-        return 1;
-    }
-
-    NoCarro* lista = NULL;
+typedef struct noCarro {
     Carro carro;
-    while (fscanf(f, "%20[^,],%30[^,],%d,%d,%f\n",
-                  carro.marca, carro.modelo, &carro.ano, &carro.km, &carro.preco) == 5) {
-        inserir_lista(&lista, carro);
-                  }
-    fclose(f);
+    struct noCarro* prox;
+} NoCarro;
 
-    salvar_binario(argv[2], lista);
-    liberar_lista(lista);
+typedef struct noArvore {
+    float chave;
+    Carro* carro;
+    struct noArvore* esq;
+    struct noArvore* dir;
+} NoArvore;
 
-    printf("Arquivo binário salvo com sucesso.\n");
+// Funções de lista
+void inserir_lista(NoCarro** head, Carro carro);
+void salvar_binario(const char* nome, NoCarro* head);
+void liberar_lista(NoCarro* head);
 
-    return 0;
-}
+// Funções de árvore
+void inserir_arvore(NoArvore** raiz, float chave, Carro* carro);
+void em_ordem_arvore(NoArvore* raiz);
+void liberar_arvore(NoArvore* raiz);
+
+#endif
